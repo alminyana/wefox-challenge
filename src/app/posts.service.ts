b04b6b7bb497757cc6859228859fcc5fb611f1d9
enric-alminyana <enric.alminyana@i2cat.net>
2018-06-23 14:33:41 +0200

    added method to get all posts

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+
+import { Post } from './model/post.model';
+import { AppConfig } from './app-config';
 
 @Injectable()
 export class PostsService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+
+  getAllPosts(){
+    let url: string = AppConfig.getPosts;
+
+    return this.http.get(url, {observe: 'body', responseType: 'json'});
+  }
+
 
 }
